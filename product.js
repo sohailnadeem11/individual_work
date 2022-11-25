2c9aa854bb55748790fd94c71e741a0be66b1b8a
SN1164 <115774815+SN1164@users.noreply.github.com>
2022-11-25 08:44:57 +0400

    Commit

---
+++ product.js
@@ -0,0 +1,92 @@
+let products = [
+    {
+                    "id": 1001,
+                    "subject": 'Maths',
+                    "location": 'London',
+                    "price": 120,
+                    "image": "images/Maths.jpeg",
+                    "availableInventory": 5,
+                    "space": 5
+    },
+    {
+                     "id": 1002,
+                     "subject": 'English',
+                     "location": 'India',
+                     "price": 80,
+                     "image": "images/English.jpeg",
+                     "availableInventory": 5,
+                     "space": 5
+},
+{
+                    "id": 1003,
+                    "subject": 'Chemistry',
+                    "location": 'America',
+                    "price": 90,
+                    "image": "images/Chemistry.jpeg",
+                    "availableInventory": 5,
+                    "space": 5
+},
+{
+                   "id": 1004,
+                   "subject": 'Physics',
+                   "location": 'Mauritius',
+                   "price": 95,
+                   "image": "images/Physics.jpeg",
+                   "availableInventory": 5,
+                   "space": 5
+},
+{
+                   "id": 1005,
+                   "subject": 'Biology',
+                   "location": 'Dubai',
+                   "price": 110,
+                   "image": "images/Biology.jpeg",
+                   "availableInventory": 5,
+                   "space": 5
+},
+{
+                  "id": 1006,
+                  "subject": 'Geography',
+                  "location": 'Canada',
+                  "price": 100,
+                  "image": "images/Geography.jpeg",
+                  "availableInventory": 5,
+                  "space": 5
+},
+{
+                  "id": 1007,
+                  "subject": 'French',
+                  "location": 'London',
+                  "price": 80,
+                  "image": "images/French.jpeg",
+                  "availableInventory": 5,
+                  "space": 5
+},
+{
+                  "id": 1008,
+                  "subject": 'Spanish',
+                  "location": 'London',
+                  "price": 85,
+                  "image": "images/Spanish.jpeg",
+                  "availableInventory": 5,
+                  "space": 5
+},
+{
+                   "id": 1009,
+                   "subject": 'German',
+                   "location": 'Dubai',
+                   "price": 75,
+                   "image": "images/German.jpeg",
+                   "availableInventory": 5,
+                   "space": 5
+},
+{
+                  "id": 1010,
+                  "subject": 'Economics',
+                  "location": 'America',
+                  "price": 140,
+                  "image": "images/Economics.jpeg",
+                  "availableInventory": 5,
+                  "space": 5
+}
+]
